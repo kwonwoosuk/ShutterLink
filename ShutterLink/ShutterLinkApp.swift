cc6bc4fe0b80cdde0f1b2fbb084416f423fd8fc8
woosuk <kevincool6@naver.com>
2025-05-10 17:59:43 +0900

    Initial Commit

---
+++ ShutterLink/ShutterLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShutterLinkApp.swift
+//  ShutterLink
+//
+//  Created by 권우석 on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ShutterLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
