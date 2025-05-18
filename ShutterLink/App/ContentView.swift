6327c864e4d219a1efbd10c6e27b3fe81f348a4b
woosuk <kevincool6@naver.com>
2025-05-18 15:26:46 +0900

    feat: kakaoAuth 설정, plist 수정(모든 위치에서 API관련 파일 untracked되도록 수정)

--- ShutterLink/App/ContentView.swift
+++ ShutterLink/App/ContentView.swift
@@ -4,21 +4,17 @@
 //
 //  Created by 권우석 on 5/10/25.
 //
-//
-//import SwiftUI
-//
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
-//            Text("Hello, world!")
-//        }
-//        .padding()
-//    }
-//}
-//
-//#Preview {
-//    ContentView()
-//}
+
+import SwiftUI
+
+struct ContentView: View {
+    @EnvironmentObject private var authState: AuthState
+    
+    var body: some View {
+        if authState.isLoggedIn {
+            HomeView()
+        } else {
+            SignInView()
+        }
+    }
+}
--- ShutterLink/App/ShutterLinkApp.swift
+++ ShutterLink/App/ShutterLinkApp.swift
@@ -6,10 +6,16 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct ShutterLinkApp: App {
     @StateObject private var authState = AuthState.shared
+    
+    init() {
+        KakaoSDK.initSDK(appKey: "6673881ea6a5986552bce8d37739b5e2")
+    }
     
     var body: some Scene {
         WindowGroup {
@@ -18,18 +24,12 @@ struct ShutterLinkApp: App {
                 .fullScreenCover(isPresented: $authState.showLoginModal) {
                     SignInView()
                 }
-        }
-    }
-}
-
-struct ContentView: View {
-    @EnvironmentObject private var authState: AuthState
-    
-    var body: some View {
-        if authState.isLoggedIn {
-            HomeView()
-        } else {
-            SignInView()
+                .onOpenURL { url in
+                    // 카카오 로그인 콜백 처리
+                    if AuthApi.isKakaoTalkLoginUrl(url) {
+                        _ = AuthController.handleOpenUrl(url: url)
+                    }
+                }
         }
     }
 }
